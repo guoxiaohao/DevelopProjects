f34822e8c5a88ad6a1690374b2aef194c5040fed
guoxiaohao <guo_xiaohao@126.com>
2019-02-13 18:05:26 +0800

    Synchronize

--- Code_Python/Code1/main.py
+++ Code_Python/Code1/main.py
@@ -1,1 +1,14 @@
-print("Hello")
+import os
+import json
+
+if __name__ == '__main__':
+    StrFile = 'ConfIni.json'
+    if os.access(StrFile, os.F_OK):
+        JsonFile = open(StrFile)
+        StrJsonFile = json.load(JsonFile)
+        StrWebSite = StrJsonFile['WebSite']
+        print(StrWebSite)
+    else:
+        print('File Not Existed')
+else:
+    print('Start Wrong')
